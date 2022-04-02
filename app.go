223a69e4a9355c02c34736006b1299ba690b6be7
alvear-and <andres.alvear@mercadolibre.com.co>
2022-04-02 06:21:49 -0500

    adding basic files

---
+++ app.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/custom", CustomServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func CustomServer(w http.ResponseWriter, r *http.Request) {
+	// modificar por el archivo que quieren mostrar
+	dat, err := os.ReadFile("files/basic.html")
+	if err != nil {
+		fmt.Fprintf(w, "No se pudo encontrar el archivo, revise la ruta")
+		return
+	}
+	fmt.Fprintf(w, string(dat))
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
